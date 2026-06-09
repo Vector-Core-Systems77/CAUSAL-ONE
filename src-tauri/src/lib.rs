8784feb324596a89ec1380f326931772522e83b3
Vector-Core-Systems77 <ywsfnsralymn@gmail.com>
2026-06-09 15:51:08 +0300

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,21 +1,21 @@
 mod h_pt_engine;
 mod commands;
 
-use h_pt_engine::HPT_Operator;
+use h_pt_engine::HptOperator;
 use std::sync::Mutex;
 use tauri::Manager;
 
-pub struct HPTState(pub Mutex<HPT_Operator>);
+pub struct HPTState(pub Mutex<HptOperator>);
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .manage(HPTState(Mutex::new(HPT_Operator::new())))
-        .invoke_handler(tauri::generate_handler![
+       .manage(HPTState(Mutex::new(HptOperator::new())))
+       .invoke_handler(tauri::generate_handler![
             commands::generate_next_zero,
             commands::get_spectrum,
             commands::get_status_cmd
         ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+       .run(tauri::generate_context!())
+       .expect("error while running tauri application");
 }
